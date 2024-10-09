f71e49604d5f2b94ec045714cee6ba476a6b4d4c
Night <dranhata@gmail.com>
2024-10-09 10:57:37 +0700

    init project

---
+++ src/main/java/com/xdpsx/auction/AuctionBeApplication.java
@@ -0,0 +1,13 @@
+package com.xdpsx.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionBeApplication.class, args);
+    }
+
+}
